ca4bfd975fa4d961ebb2f571140c04217d28d389
MatteoDuco <matteoducoli.is@gmail.com>
2021-03-03 15:59:13 +0100

    minor change for deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:MatteoDuco/MatteoDuco.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:MatteoDuco/CovidGraphItalia.git main:github-pages
+git push -f git@github.com:MatteoDuco/CovidGraphItalia.git master:gh-pages
 
 cd -
